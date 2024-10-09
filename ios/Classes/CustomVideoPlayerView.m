24c2026fdfa43df9f62d548955b704a24ca8fce9
hari krishna bista <harikrishnabista7@gmail.com>
2024-10-09 14:44:54 +0545

    optimized by reusing context

--- ios/Classes/CustomVideoPlayerView.m
+++ ios/Classes/CustomVideoPlayerView.m
@@ -7,6 +7,10 @@
 
 #import "CustomVideoPlayerView.h"
 
+@interface CustomVideoPlayerView ()
+@property (nonatomic, strong) CIContext *ciContext; // Property to hold the CIContext
+@end
+
 @implementation CustomVideoPlayerView
 
 // Override the setter for player property
@@ -15,8 +19,8 @@
 
     if (player.currentItem) {
         NSDictionary *outputSettings = @{
-            (__bridge NSString *)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA),
-            (__bridge NSString *)kCVPixelBufferIOSurfacePropertiesKey : @{}, // Empty dictionary for IOSurface properties
+                (__bridge NSString *)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_32BGRA),
+                (__bridge NSString *)kCVPixelBufferIOSurfacePropertiesKey: @{}, // Empty dictionary for IOSurface properties
         };
         self.playerItemOutput = [[AVPlayerItemVideoOutput alloc] initWithOutputSettings:outputSettings];
         [player.currentItem addOutput:self.playerItemOutput];
@@ -52,24 +56,26 @@
     self.displayLink.preferredFramesPerSecond = 60; // Default 60fps for smoother playback
 }
 
-
 // Function to extract the color space from a pixel buffer
 CGColorSpaceRef getColorSpaceFromPixelBuffer(CVPixelBufferRef pixelBuffer) {
     // Get the attachments dictionary from the pixel buffer
     CFDictionaryRef attachments = CVBufferCopyAttachments(pixelBuffer, kCVAttachmentMode_ShouldPropagate);
-    
+
     // Check if there's a color space attachment
     CGColorSpaceRef colorSpace = NULL;
+
     if (attachments != NULL) {
         // Look for the kCVImageBufferCGColorSpaceKey
         CFTypeRef colorSpaceAttachment = CFDictionaryGetValue(attachments, kCVImageBufferCGColorSpaceKey);
+
         if (colorSpaceAttachment != NULL) {
             colorSpace = (CGColorSpaceRef)colorSpaceAttachment;
             CFRetain(colorSpace); // Retain the color space to return it
         }
+
         CFRelease(attachments); // Release the attachments dictionary
     }
-    
+
     return colorSpace;
 }
 
@@ -85,29 +91,28 @@ CGColorSpaceRef getColorSpaceFromPixelBuffer(CVPixelBufferRef pixelBuffer) {
         CVPixelBufferRef pixelBuffer = [self.playerItemOutput copyPixelBufferForItemTime:currentTime itemTimeForDisplay:nil];
 
         if (pixelBuffer) {
-            CGColorSpaceRef colorSpace;
-            
-            colorSpace = getColorSpaceFromPixelBuffer(pixelBuffer);
+            CGColorSpaceRef colorSpace = getColorSpaceFromPixelBuffer(pixelBuffer);
 
             if (colorSpace == NULL) {
                 colorSpace = CGColorSpaceCreateDeviceRGB();
             }
-            
-            // Create a CIContext with sRGB color space
-          //  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-            CIContext *context = [CIContext contextWithOptions:@{ kCIContextWorkingColorSpace: (__bridge id)colorSpace }];
+
+            // Create CIContext only if it hasn't been created yet
+            if (!self.ciContext) {
+                self.ciContext = [CIContext contextWithOptions:@{ kCIContextWorkingColorSpace: (__bridge id)colorSpace }];
+            }
 
             // Convert the pixel buffer to a CIImage
             CIImage *ciImage = [CIImage imageWithCVPixelBuffer:pixelBuffer];
 
             // Create CGImage from CIImage with the proper color space
-            CGImageRef cgImage = [context createCGImage:ciImage fromRect:ciImage.extent];
+            CGImageRef cgImage = [self.ciContext createCGImage:ciImage fromRect:ciImage.extent];
 
             // Release the color space when done (if you created it)
             if (colorSpace != NULL) {
                 CGColorSpaceRelease(colorSpace);
             }
-            
+
             if (cgImage) {
                 UIImage *uiImage = [UIImage imageWithCGImage:cgImage];
 
